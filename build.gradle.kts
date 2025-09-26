a4895d3a24045bc095325fa9c77c23fa389d2846
Dmitry <dmitrijs.lobanovskis@ai71.ai>
2025-09-26 16:18:53 +0400

    Unblocking ci/cd

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:redis")
 }
 
 tasks.withType<Test> {
